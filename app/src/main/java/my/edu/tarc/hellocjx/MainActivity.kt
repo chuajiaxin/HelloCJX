1114d855c774160141306be5d8a6940869c33e36
anreh <anurehka@gmail.com>
2023-02-16 18:02:35 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/hellocjx/MainActivity.kt
@@ -0,0 +1,37 @@
+package my.edu.tarc.hellocjx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+//Instant Name: Class Name
+class MainActivity : AppCompatActivity() {
+    //onCreate = main function
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //Load UI to memory
+        //R = resource class
+        setContentView(R.layout.activity_main)
+
+        //Write UI Handler here
+        //Variable declaration; 1) val = value, 2) var = variable
+        val textViewName: TextView = findViewById(R.id.textViewName)
+        val imageViewQRCode = findViewById<ImageView>(R.id.imageViewQRCode)
+        val buttonShow: Button = findViewById(R.id.buttonShow)
+        val buttonHide: Button = findViewById(R.id.buttonHide)
+
+        buttonShow.setOnClickListener {
+            textViewName.visibility = View.VISIBLE
+            imageViewQRCode.visibility = View.VISIBLE
+        }
+
+        buttonHide.setOnClickListener {
+            textViewName.visibility = View.INVISIBLE
+            imageViewQRCode.visibility = View.INVISIBLE
+        }
+    }
+}
